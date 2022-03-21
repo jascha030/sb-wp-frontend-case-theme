edd6091a0e347cf0e1fe998af30820413d7b6bc1
Jascha030 <contact@jaschavanaalst.nl>
2022-03-21 20:53:35 +0100

    🔧 Start theme definitions file with theme_supports config.

--- includes/definitions/theme.php
+++ includes/definitions/theme.php
@@ -1,1 +1,24 @@
 <?php
+
+declare(strict_types=1);
+
+return [
+    'theme.supports' => [
+        'custom-background'                   => false,
+        'customize-selective-refresh-widgets' => false,
+        'custom-header-uploads'               => true,
+        'custom-header'                       => true,
+        'title-tag'                           => true,
+        'post-thumbnails'                     => true,
+        'post-formats'                        => [],
+        'html5'                               => true,
+        'custom-logo'                         => [
+            'height'               => 300,
+            'width'                => 71,
+            'flex-height'          => true,
+            'flex-width'           => true,
+            'header-text'          => ['site-title', 'site-description'],
+            'unlink-homepage-logo' => true,
+        ],
+    ],
+];
