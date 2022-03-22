94d2309331a99098a2336fc18c257766860febcb
Jascha030 <contact@jaschavanaalst.nl>
2022-03-22 21:42:29 +0100

    ✨ Add twig helpers.

--- includes/helpers/twig.php
+++ includes/helpers/twig.php
@@ -2,14 +2,30 @@
 
 declare(strict_types=1);
 
-namespace Jascha030\WpFrontendCaseTheme\Helpers;
+namespace Jascha030\WpFrontendCaseTheme\Helpers\Twig;
 
+use DI\NotFoundException;
+use Jascha030\Twig\TwigServiceInterface;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use function Jascha030\WpFrontendCaseTheme\Helpers\Container\service;
+
+/**
+ * @throws ContainerExceptionInterface
+ * @throws NotFoundExceptionInterface
+ * @throws NotFoundException
+ */
 function asString(string $template, array $context = []): string
 {
-    return '';
+    return service(TwigServiceInterface::class)?->renderString($template, $context) ?? '';
 }
 
+/**
+ * @throws ContainerExceptionInterface
+ * @throws NotFoundExceptionInterface
+ * @throws NotFoundException
+ */
 function render(string $template, array $context = []): void
 {
-    echo '';
+    service(TwigServiceInterface::class)?->render($template, $context);
 }
