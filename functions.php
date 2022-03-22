ec92ba2f6208a9665ea9ed66ffef25746c9c3c06
Jascha030 <contact@jaschavanaalst.nl>
2022-03-22 22:03:54 +0100

    ✨ Add theme initialization with theme_supports.

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Jascha030\WpFrontendCaseTheme\Theme;
 
 use RuntimeException;
+use function Jascha030\WpFrontendCaseTheme\Helpers\Theme\themeSupports;
 
 /**
  * Require theme/bootstrapping functions.
@@ -18,18 +19,36 @@ require_once __DIR__ . '/includes/bootstrap.php';
  */
 load(__DIR__);
 
+/**
+ * Abort after autoloaders if outside of WordPress context.
+ */
 if (! defined('ABSPATH')) {
     return;
 }
 
+function addThemeSupports(): void
+{
+    foreach (themeSupports() as $feature => $value) {
+        if (false === $value) {
+            continue;
+        }
+
+        if (true === $value) {
+            \add_theme_support($feature);
+
+            continue;
+        }
+
+        \add_theme_support($feature, $value);
+    }
+}
+
 /**
  * Initialize theme.
- *
- * @return void
  */
 function init(): void
 {
-    // Todo: theme initialization.
+    addThemeSupports();
 }
 
 add_action('after_setup_theme', 'init', 10);
