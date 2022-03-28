dc377c81660452ba8095f0692bead3c47a5697e3
Jascha030 <contact@jaschavanaalst.nl>
2022-03-28 23:49:24 +0200

    🔧 Move simple theme "supports" definition to theme base config.

--- includes/config.php
+++ includes/config.php
@@ -9,6 +9,24 @@ return [
     ],
     'scripts' => [
     ],
+    'supports' => [
+        'custom-background'                   => false,
+        'customize-selective-refresh-widgets' => false,
+        'custom-header-uploads'               => true,
+        'custom-header'                       => true,
+        'title-tag'                           => true,
+        'post-thumbnails'                     => true,
+        'post-formats'                        => [],
+        'html5'                               => true,
+        'custom-logo'                         => [
+            'height'               => 300,
+            'width'                => 71,
+            'flex-height'          => true,
+            'flex-width'           => true,
+            'header-text'          => ['site-title', 'site-description'],
+            'unlink-homepage-logo' => true,
+        ],
+    ],
     'autoload_paths' => static fn (string $themeRoot): array => defined('ABSPATH')
         ? [$themeRoot]
         : [
