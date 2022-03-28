29b7f415ec26eff6e43bcbe33bee8da8c6d90208
Jascha030 <contact@jaschavanaalst.nl>
2022-03-28 23:48:49 +0200

    🚧 Error handling for now.

--- includes/helpers/theme.php
+++ includes/helpers/theme.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Jascha030\WpFrontendCaseTheme\Helpers\Theme;
 
 use DI\NotFoundException;
+use Exception;
 use Jascha030\WpFrontendCaseTheme\Theme\ThemeInterface;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\NotFoundExceptionInterface;
@@ -13,21 +14,28 @@ use function Jascha030\WpFrontendCaseTheme\Helpers\Container\service;
 /**
  * Retrieve the bound ThemeInterface implementation.
  *
- * @throws ContainerExceptionInterface|NotFoundException
- * @throws NotFoundExceptionInterface
+ * @noinspection PhpUnhandledExceptionInspection
  */
 function getTheme(): ThemeInterface
 {
-    return service(ThemeInterface::class);
+    try {
+        return service(ThemeInterface::class);
+    } catch (Exception|NotFoundException|NotFoundExceptionInterface|ContainerExceptionInterface $e) {
+        if (! defined('ABSPATH')) {
+            throw $e;
+        }
+
+        \error_log($e->getMessage());
+        \wp_die($e->getMessage());
+    }
 }
 
 /**
  * Retrieve the theme instance, alias of `getTheme()`.
  *
- * @throws ContainerExceptionInterface|NotFoundException
- * @throws NotFoundExceptionInterface
+ * @see          ThemeInterface
  *
- * @see ThemeInterface
+ * @noinspection PhpUnhandledExceptionInspection
  */
 function theme(): ThemeInterface
 {
