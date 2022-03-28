a4b96d5b71c9dd6946178e7d95bf5024da9fddfe
Jascha030 <contact@jaschavanaalst.nl>
2022-03-29 00:32:53 +0200

    ♻️ Realise try/catch is bogus, thanks to unittest.

--- app/Theme/Traits/ThemeRootProviderTrait.php
+++ app/Theme/Traits/ThemeRootProviderTrait.php
@@ -4,17 +4,14 @@ declare(strict_types=1);
 
 namespace Jascha030\WpFrontendCaseTheme\Theme\Traits;
 
-use Exception;
 use function Jascha030\WpFrontendCaseTheme\Helpers\Theme\theme;
 
 trait ThemeRootProviderTrait
 {
     public function getThemeRoot(bool $preferUri = true): string
     {
-        try {
-            return $preferUri ? theme()->getRootUri() : theme()->getRootDir();
-        } catch (Exception) {
-            return dirname(__FILE__, 4);
-        }
+        return $preferUri
+            ? theme()->getRootUri() ?? theme()->getRootDir()
+            : theme()->getRootDir();
     }
 }
