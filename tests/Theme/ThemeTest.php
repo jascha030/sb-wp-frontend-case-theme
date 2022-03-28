08200e9538959af821c050fc5646456da8e9802b
Jascha030 <contact@jaschavanaalst.nl>
2022-03-29 00:11:49 +0200

    🔧 load includes/bootstrap.php in unittest config.

---
+++ tests/Theme/ThemeTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Jascha030\WpFrontendCaseTheme\Tests\Theme;
+
+use Jascha030\WpFrontendCaseTheme\Theme\Theme;
+use PHPUnit\Framework\TestCase;
+
+class ThemeTest extends TestCase
+{
+
+}
